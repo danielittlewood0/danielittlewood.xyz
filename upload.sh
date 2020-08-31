ef9335d4008b2c57da39a1e768f5c6e81a114d3f
Daniel Littlewood <danielittlewood@gmail.com>
2020-08-31 22:41:27 +0100

    [Development] Use rsync for site upload

--- upload.sh
+++ upload.sh
@@ -1,3 +1,2 @@
 # Requires my ssh keys and NearlyFreeSpeech password
-scp -r public/* danielittlewood_danielittlewood@ssh.phx.nearlyfreespeech.net:/home/public
-
+rsync -av --delete public/ danielittlewood_danielittlewood@ssh.phx.nearlyfreespeech.net:/home/public
